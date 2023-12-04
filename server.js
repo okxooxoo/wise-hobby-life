6c03075af0f93a15434837785dfcf6c2ff74b564
okxooxoo <okxooxoo@naver.com>
2023-12-05 01:33:19 +0900

    ✨ feat: 메인 페이지 생성

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ app.use(express.static(__dirname + "/public"));
 app.listen(8080, function () {
   console.log("포트 8080으로 서버 대기 중...");
 });
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/home.html");
+});
