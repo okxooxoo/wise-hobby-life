24811cda1714f1da8caf385d6362ff5137ee966e
okxooxoo <okxooxoo@naver.com>
2023-12-05 00:15:20 +0900

    ✨ feat: express 서버 생성

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+app.listen(8080, function () {
+  console.log("포트 8080으로 서버 대기 중...");
+});
