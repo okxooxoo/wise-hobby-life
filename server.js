206f63bb490810190582b8e3ec79aab34ecdd6ef
okxooxoo <okxooxoo@naver.com>
2023-12-06 18:39:49 +0900

    ✨ feat: 회원가입 & 로그인 페이지 추가

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 
 app.listen(8080, function () {
   console.log("포트 8080으로 서버 대기 중...");
 });
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/home.html");
+  res.render('home.ejs');
+});
+
+app.get("/signup", function(req, res) {
+  res.render('signup.ejs');
+});
+
+app.get("/login", function(req, res) {
+  res.render('login.ejs');
 });
